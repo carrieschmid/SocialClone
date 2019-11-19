12c63c656243e8f0fcbe6594f9d213817b0affa3
Student <example@example.com>
2019-11-18 16:38:14 -0800

    Add notes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,9 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+    //super accesses the React.Component constructor
     this.state = {
+      //all state value key-value pairs go here
       newsFeedList: [
         {
           userName: "name1",
@@ -24,24 +26,34 @@ class App extends React.Component {
 
     this.handleAddingNewSearchTerm =
       this.handleAddingNewSearchTerm.bind(this);
+      //Because handler is not bound by default, we must manually bind it in our constructor to define its "this"
+      //constructor() does know what this is, because it defines and creates each instance of our component. That's why we may successfully call things like this.state = {}; in constructor().
+      //anytime a method in a class-based component needs to reference this it must be bound.
   }
 
   handleAddingNewPostToList(newPost) {
     var newNewsFeedList = this.state.newsFeedList.slice();
     newNewsFeedList.push(newPost);
     this.setState({ newsFeedList: newNewsFeedList });
+    //update our state define its new value
   }
+  //this is the handler for the event of submitting the post
+  //because its a class method, it's not bound
 
   handleAddingNewSearchTerm(newSearch) {
     this.setState({ currentSearchTerm: newSearch });
     console.log(this.state.currentSearchTerm);
   }
+   //this is the handler for the event of submitting the search
 
   render() {
+    //Like any other method, we may include logic before the return statement.(NewTicketControl.jsx)
     return (
       <div className="App container">
         <div className="row" >
           <NavBar handleAddingNewSearchTerm={this.handleAddingNewSearchTerm}/>
+          {/* A method executed in response to an event is called an event handler. The event is the rendering of the NavBar.
+          Here, we're passing the handleTroubleShootingConfirmation() method from App.jsx into NavBar as a prop named handleTroubleShootingConfirmation. */}
         </div>
 
         <div className="row">
